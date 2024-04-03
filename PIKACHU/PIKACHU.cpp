7dacf45eac04d0490c83cf0a700dd7f53566943c
huynhgiaau27112005 <huynhgiaau27112005@gmail.com>
2024-04-04 01:22:24 +0700

    4/4

--- PIKACHU/PIKACHU.cpp
+++ PIKACHU/PIKACHU.cpp
@@ -28,13 +28,7 @@ int main()
     PlaySound(sound_Correct);
 
     LoginWindow();
-    /* Những phần Hà code Âu đã chuyển vào hàm void Play_OPTION(int boardWidth, int boardLength) trong PLAY.cpp nha
-    * Trong hàm Âu có để 2 tham số boardWidth với boardLength là kích thước ma trận, có gì Hà chỉnh code để ma trận linh hoạt thay đổi kích thước
-    * Với cái int c[][12] Hà chỉnh lại thành int** c rồi cấp phát kích thước nha do thầy kêu đồ án này quan trọng con trỏ
-    */
     UnloadAllSound();
     CloseAudioDevice();
     return 0;
 }
-
-// Au sua 123
